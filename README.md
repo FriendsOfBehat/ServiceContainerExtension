b0fcd3cc51ce82d368b4d85498a113ae74289591
Kamil Kokot <kamil@kokot.me>
2016-07-27 23:48:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Service Container Extension [![License](https://img.shields.io/packagist/l/friends-of-behat/service-container-extension.svg)](https://packagist.org/packages/friends-of-behat/service-container-extension) [![Version](https://img.shields.io/packagist/v/friends-of-behat/service-container-extension.svg)](https://packagist.org/packages/friends-of-behat/service-container-extension) [![Build status on Linux](https://img.shields.io/travis/FriendsOfBehat/ServiceContainerExtension/master.svg)](http://travis-ci.org/FriendsOfBehat/ServiceContainerExtension) [![Scrutinizer Quality Score](https://img.shields.io/scrutinizer/g/FriendsOfBehat/ServiceContainerExtension.svg)](https://scrutinizer-ci.com/g/FriendsOfBehat/ServiceContainerExtension/)
+
+Allows to declare own services inside Behat container without writing an extension.
+
+## Usage
+
+1. Install it:
+
+```bash
+$ composer require friends-of-behat/service-container-extension --dev
+```
+
+2. Enable and configure:
+
+```yaml
+default:
+    # ...
+    extensions:
+        FriendsOfBehat\ServiceContainerExtension:
+            imports:
+                - "features/bootstrap/config/services.xml"
+                - "features/bootstrap/config/services.yml"
+```
---
+++ src/ServiceContainer/ServiceContainerExtension.php
@@ -0,0 +1,85 @@
+<?php
+
+
+/*
+ * This file is part of the ServiceContainerExtension package.
+ *
+ * (c) FriendsOfBehat
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfBehat\ServiceContainerExtension\ServiceContainer;
+
+use Behat\Testwork\ServiceContainer\Extension;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * @author Kamil Kokot <kamil@kokot.me>
+ */
+final class ServiceContainerExtension implements Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigKey()
+    {
+        return 'fob_service_container';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(ExtensionManager $extensionManager)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        $builder
+            ->children()
+                ->arrayNode('imports')
+                    ->performNoDeepMerging()
+                    ->prototype('scalar')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ContainerBuilder $container, array $config)
+    {
+        $fileLocator = new FileLocator($container->getParameter('paths.base'));
+
+        $loader = new DelegatingLoader(new LoaderResolver([
+            new XmlFileLoader($container, $fileLocator),
+            new YamlFileLoader($container, $fileLocator),
+            new PhpFileLoader($container, $fileLocator),
+        ]));
+
+        foreach ($config['imports'] as $file) {
+            $loader->load($file);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+
+    }
+}
