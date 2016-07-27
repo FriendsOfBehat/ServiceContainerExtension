3736298f6d49cdd311be6b7dbf5faf3eefd4267c
Kamil Kokot <kamil@kokot.me>
2016-07-27 23:53:40 +0200

    Initialize Behat

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the ServiceContainerExtension package.
+ *
+ * (c) FriendsOfBehat
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Behat\Behat\Context\Context;
+
+/**
+ * @author Kamil Kokot <kamil@kokot.me>
+ */
+final class FeatureContext implements Context
+{
+
+}
